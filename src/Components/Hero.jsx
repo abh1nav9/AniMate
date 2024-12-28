4b77c2149ae58e3ba35b8963ffb6de6ca2238b97
abh1nav9 <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-12-28 20:55:51 +0530

    Added : Hero Texts

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -66,6 +66,21 @@ const Hero = () => {
               onLoadedData={handleVideoLoad}
             />
           </div>
+          <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+            G<b>A</b>MING
+          </h1>
+          <div className="absolute left-0 top-0 z-40 size-full">
+            <div className="mt-24 px-5 sm:px-10">
+              <h1 className="special-font hero-heading text-blue-100">
+                REDEFI<b>N</b>E
+              </h1>
+              <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+                entre the metagame layer
+                <br />
+                unleash the play economy
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
